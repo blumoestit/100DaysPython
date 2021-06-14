53fa1b26f547869915d4205df8bde4d10d02fb2d
blumoestit <blumoestit@gmail.com>
2021-06-14 14:05:26 +0200

    Add day 1: simple concat.

---
+++ simple_concat.py
@@ -0,0 +1,18 @@
+#### BAND NAME GENERATOR ####
+
+# 1. Greeting 
+print("Welcome to the Band Name Generator!")
+
+# 2. Ask the user for the city that they grow up in
+city = input("Which city did you grow up in?\n")
+print(city)
+
+# 3. Ask user of the name of the pet
+pet = input("What ist the name of your pet?\n")
+print(pet)
+
+# 4. Combine the names
+print("your band name could be: " + city + " " + pet)
+
+
+
