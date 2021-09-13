084f01cbddb82c2c2d8056cbd2f47f0ff664beec
blumoestit <blumoestit@gmail.com>
2021-09-13 14:40:06 +0200

    Add QR code mini project

---
+++ QR_Code/QR.py
@@ -0,0 +1,13 @@
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=5,
+    border=4,
+)
+data = 'Peppermint. Uses: indigestion, nausea. Steep time: 5-10 mins.'
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="green", back_color="white")
+img.save('peppermint.png')
